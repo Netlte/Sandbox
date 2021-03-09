7a5cf04a5259b08ddd2c9c6809b43b4578f6f6ed
Tomáš Holan <tomas@holan.dev>
2021-03-09 19:53:23 +0100

    Fixed PHPStan for lvl max

--- app/Presenters/Error4xxPresenter.php
+++ app/Presenters/Error4xxPresenter.php
@@ -9,7 +9,7 @@ final class Error4xxPresenter extends BasePresenter {
 
 	public function startup(): void {
 		parent::startup();
-		if (!$this->getRequest()->isMethod(Nette\Application\Request::FORWARD)) {
+		if ($this->getRequest() === null || !$this->getRequest()->isMethod(Nette\Application\Request::FORWARD)) {
 			$this->error();
 		}
 	}
@@ -19,6 +19,6 @@ final class Error4xxPresenter extends BasePresenter {
 		// load template 403.latte or 404.latte or ... 4xx.latte
 		$file = __DIR__ . "/templates/Error/{$exception->getCode()}.latte";
 		$file = \is_file($file) ? $file : __DIR__ . '/templates/Error/4xx.latte';
-		$this->template->setFile($file);
+		$this->getTemplate()->setFile($file);
 	}
 }
--- app/Presenters/ErrorPresenter.php
+++ app/Presenters/ErrorPresenter.php
@@ -32,7 +32,8 @@ final class ErrorPresenter implements Nette\Application\IPresenter {
 
 		$this->logger->log($e, ILogger::EXCEPTION);
 		return new Responses\CallbackResponse(function (Http\IRequest $httpRequest, Http\IResponse $httpResponse): void {
-			if (\preg_match('#^text/html(?:;|$)#', (string) $httpResponse->getHeader('Content-Type'))) {
+			$result = \preg_match('#^text/html(?:;|$)#', (string) $httpResponse->getHeader('Content-Type'));
+			if ($result === false || $result < 1) {
 				require __DIR__ . '/templates/Error/500.phtml';
 			}
 		});
